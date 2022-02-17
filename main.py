cf3178e7b96fb4c146781aa0c6bfa97ad3562a77
John Adams Jr <john.c.adams32@gmail.com>
2022-02-16 20:29:54 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+BACKGROUND_COLOR = "#B1DDC6"
+
+# TODO: Build the UI
+# TODO: Build button functions "needs_study", and known_phrase
+# TODO: import data from the csv
+# TODO: categorize known and unknown phrases
